96172ff7e1d8be1878b0015caf35a92de9161e0c
AndersonBeltran <abeltran@appdev.yearup.org>
2024-11-06 15:01:49 -0500

    Mountain page done

--- scripts/mountains.js
+++ scripts/mountains.js
@@ -1,7 +1,24 @@
 "use strict";
 
-function loadMountains(params) {
-  for (const mountain of mountainsArray) {
-    console.log(mountain);
-  }
+const mountainSelect = document.getElementById("mountainSelect");
+mountainsArray.filter((mountain, index) => {
+  const option = document.createElement("option");
+  option.value = index;
+  option.textContent = mountain.name;
+  mountainSelect.appendChild(option);
+});
+
+function displayMountainInfo() {
+  const selectedMountain = mountainsArray[parseInt(mountainSelect.value)];
+  document.getElementById("mountainImage").src = "images/" + selectedMountain.img;
+  document.getElementById("mountainName").textContent = selectedMountain.name;
+  document.getElementById("effort").textContent = "Eeffort: "  + selectedMountain.effort;
+  document.getElementById("description").textContent = selectedMountain.desc;
+  document.getElementById("elevation").textContent =  "Elevation: "  + selectedMountain.elevation;
 }
+
+
+
+
+
+
--- scripts/parkTypeData.js
+++ scripts/parkTypeData.js
@@ -25,40 +25,42 @@ for (const park of parkTypesArray) {
 }
 
 function filterParksType() {
-  let selectedpark = parksDropdown.value;
-  let parkTypesArray = nationalParksArray.filter((park) => park.type == selectedpark);
+  let selectedPark = parksDropdown.value;
+  console.log(selectedPark);
   parksTBody.innerHTML = "";
 
-  for (const Parks of parkTypesArray) {
-    let tr = document.createElement("tr");
-    parksTBody.appendChild(tr);
+  for (const Parks of nationalParksArray) {
+    if (Parks.LocationName.includes(selectedPark)) {
+      let tr = document.createElement("tr");
+      parksTBody.appendChild(tr);
 
-    let td1 = document.createElement("td");
-    td1.innerText = Parks.LocationName ? Parks.LocationName : "N/A";
-    tr.appendChild(td1);
+      let td1 = document.createElement("td");
+      td1.innerText = Parks.LocationName ? Parks.LocationName : "N/A";
+      tr.appendChild(td1);
 
-    let td2 = document.createElement("td");
-    td2.innerText = Parks.Address ? Parks.Address : "N/A";
-    tr.appendChild(td2);
+      let td2 = document.createElement("td");
+      td2.innerText = Parks.Address ? Parks.Address : "N/A";
+      tr.appendChild(td2);
 
-    let td3 = document.createElement("td");
-    td3.innerText = Parks.City ? Parks.City : "N/A";
-    tr.appendChild(td3);
+      let td3 = document.createElement("td");
+      td3.innerText = Parks.City ? Parks.City : "N/A";
+      tr.appendChild(td3);
 
-    let td4 = document.createElement("td");
-    td4.innerText = Parks.State ? Parks.State : "N/A";
-    tr.appendChild(td4);
+      let td4 = document.createElement("td");
+      td4.innerText = Parks.State ? Parks.State : "N/A";
+      tr.appendChild(td4);
 
-    let td5 = document.createElement("td");
-    td5.innerText = Parks.Phone ? Parks.Phone : "N/A";
-    tr.appendChild(td5);
+      let td5 = document.createElement("td");
+      td5.innerText = Parks.Phone ? Parks.Phone : "N/A";
+      tr.appendChild(td5);
 
-    let td6 = document.createElement("td");
-    td6.innerText = Parks.ZipCode ? Parks.ZipCode : "N/A";
-    tr.appendChild(td6);
+      let td6 = document.createElement("td");
+      td6.innerText = Parks.ZipCode ? Parks.ZipCode : "N/A";
+      tr.appendChild(td6);
 
-    let td7 = document.createElement("td");
-    td7.innerText = Parks.Visit ? Parks.Visit : "N/A";
-    tr.appendChild(td7);
+      let td7 = document.createElement("td");
+      td7.innerText = Parks.Visit ? Parks.Visit : "N/A";
+      tr.appendChild(td7);
+    }
   }
 }
